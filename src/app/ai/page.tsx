a6cf606692fbe4dbae90329a1b1da8dfe03c91fc
husamXA1 <husamxm0@gmail.com>
2024-12-29 09:39:33 +0200

    embedded botpress chatbot

--- src/app/ai/page.tsx
+++ src/app/ai/page.tsx
@@ -1,3 +1,5 @@
 export default function ChatBot() {
-  return <h1>ChatBot</h1>;
+  return (
+    <iframe className="w-full h-screen" src="https://cdn.botpress.cloud/webchat/v2.2/shareable.html?configUrl=https://files.bpcontent.cloud/2024/12/29/05/20241229051843-AG20V434.json" />
+  );
 }
